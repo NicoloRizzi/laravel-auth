bdcb9b131d75bb6f99def590963ff324567be3bd
Nicolo <nicolorizzi1996@gmail.com>
2020-06-25 18:12:36 +0200

    auth

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,9 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('guest.welcome');
+})->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+/**
+ * ROUTING ADMIN
+ */
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->group(function () {
+        Route::get('admin/home', 'HomeController@index')->name('home');
+});
