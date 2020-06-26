cac21dc40ec8a59c2f61cb7a32e15e3138d695b3
Nicolo <nicolorizzi1996@gmail.com>
2020-06-26 15:20:09 +0200

    routing admin

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -19,12 +19,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-/**
- * ROUTING ADMIN
- */
+//ADMIN
 Route::prefix('admin')
     ->namespace('Admin')
     ->middleware('auth')
     ->group(function () {
-        Route::get('admin/home', 'HomeController@index')->name('home');
-});
+        Route::get('/home', 'HomeController@index')->name('admin.home');
+    });
