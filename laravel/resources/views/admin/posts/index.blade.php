e6f6f8da16d539b36224c5fc282f05d785f9ace1
Nicolo <nicolorizzi1996@gmail.com>
2020-06-26 16:51:48 +0200

    showed a single post

--- laravel/resources/views/admin/posts/index.blade.php
+++ laravel/resources/views/admin/posts/index.blade.php
@@ -5,7 +5,7 @@
     <h1 class="mb-4">Blog Archive</h1>
 
     <table class="table">
-        <th>
+        <thead>
             <tr>
                 <th>ID</th>
                 <th>Title</th>
@@ -13,15 +13,16 @@
                 <th>Updated at</th>
                 <th colspan="3"></th>
             </tr>
-            <tbody>
-                @foreach ($posts as $post)
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
                 <tr>
                     <td>{{ $post->id }}</td>
                     <td>{{ $post->title }}</td>
                     <td>{{ $post->created_at }}</td>
                     <td>{{ $post->updated_at }}</td>
                     <td>
-                        <a href="" class="btn btn-success">Show</a>
+                    <a href="{{ route('admin.posts.show', $post->id) }}" class="btn btn-success">Show</a>
                     </td>
                     <td>
                         <a href="" class="btn btn-primary">Edit</a>
@@ -30,9 +31,8 @@
                         <a href="" class="btn btn-danger">Delete</a>
                     </td>
                 </tr>
-                @endforeach
-            </tbody>
-        </th>
+            @endforeach
+        </tbody>
     </table>
     <div class="wrap-pagination mt-5 d-flex justify-content-end">
         {{$posts->links()}}
