c39e2fec3495e708e37375c5085742cc57fdfd04
Nicolo <nicolorizzi1996@gmail.com>
2020-07-01 11:29:52 +0200

    FIX ENCTYPE

--- laravel/resources/views/admin/posts/edit.blade.php
+++ laravel/resources/views/admin/posts/edit.blade.php
@@ -14,7 +14,7 @@
         </div>
     @endif
 
-    <form action="{{ route('admin.posts.update', $post->id) }}" method="POST">
+    <form action="{{ route('admin.posts.update', $post->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PATCH')
 
@@ -27,7 +27,7 @@
             <textarea class="form-control" name="body" id="body">{{ old('body', $post->body) }}</textarea>
         </div>
         <div class="form-group">
-            <label for="path_img">Post Image</label>
+            <label for="path_img">Post Image:</label>
             @isset($post->path_img)
                 <img width="200" src="{{asset('storage/' . $post->path_img)}}" alt="{{$post->title}}">
                 <h6 class="mt-2">Change:</h6>
